cd5b26a91027f75e968856cee437c52b3ca3d367
NagaSwethaSettipalli <tilak.swetha@gmail.com>
2021-02-12 18:39:00 +0530

    Protractor project First commit

---
+++ spec.js
@@ -0,0 +1,8 @@
+describe('Protractor Demo', function()
+{
+    it('should have a title', function()
+    {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+});
