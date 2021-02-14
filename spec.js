d5c1228c20700fca2d8192cd46772642f55a2c9c
NagaSwethaSettipalli <tilak.swetha@gmail.com>
2021-02-14 12:14:56 +0530

    Added new Test

--- spec.js
+++ spec.js
@@ -21,3 +21,18 @@ describe('Enter name in textbox', function()
          expect(text.getText()).toEqual('Hello Swetha!');
      });
 });
+
+describe('multiple elements',function()
+{
+    it('should add 2 numbers', function()
+    {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        element(by.model('first')).sendKeys(2);
+        element(by.model("second")).sendKeys(5);
+
+        element(by.id("gobutton")).click();
+
+        expect(element(by.className('ng-binding')).getText()).toEqual('7');
+
+    });
+});
