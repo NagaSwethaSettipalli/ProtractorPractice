0edac0b53f1ca69eb962fd27918bf2c9f6e5dd69
NagaSwethaSettipalli <tilak.swetha@gmail.com>
2021-02-14 11:47:40 +0530

    Added command to maximize browser window

--- conf.js
+++ conf.js
@@ -4,4 +4,14 @@ exports.config = {
     seleniumAddress: 'https://localhost:4444/wd/hub',
     specs: ['spec.js'], //Name of the Specfile
     directConnect: true,
+    multiCapabilities: [
+      {
+      'browserName': 'chrome',
+      'chromeOptions': {
+        prefs: {
+          'profile.managed_default_content_settings.notifictions': 1
+        }
+      }
+    }
+    ],
   }
--- spec.js
+++ spec.js
@@ -1,8 +1,23 @@
+const { browser, element } = require("protractor");
+
 describe('Protractor Demo', function()
 {
     it('should have a title', function()
     {
         browser.get('http://juliemr.github.io/protractor-demo/');
+        browser.driver.manage().window().maximize();
         expect(browser.getTitle()).toEqual('Super Calculator');
     });
+});
+
+describe('Enter name in textbox', function()
+{
+     it('Should enter name as Swetha', function()
+     {
+         browser.get('https://angularjs.org/');
+         browser.driver.manage().window().maximize();
+         element(by.model('yourName')).sendKeys('Swetha');
+         var text = element(by.xpath('/html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
+         expect(text.getText()).toEqual('Hello Swetha!');
+     });
 });
